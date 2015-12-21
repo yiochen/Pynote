d01a0ad0944602948546ccc3a5c168d4914e77db
yiochen <chen.yiou1992@gmail.com>
2015-12-21 18:19:32 -0500

    multi level/color debuging

--- debug.py
+++ debug.py
@@ -1,6 +1,25 @@
+NONE=0
 ERROR=1
-DEBUG=2
-VERBOSE=3
+WARNING=2
+DEBUG=3
+VERBOSE=4
 __debugLevel=0
 def initDebug(level):
-    __debugLevel=level
+    global __debugLevel
+    __debugLevel=level or NONE
+    print __debugLevel
+
+def pdebug(message):
+    if __debugLevel>=DEBUG:
+        print "\033[94m%s%s\033[0m"%("DEBUG".ljust(10),str(message))
+
+def pinfo(message):
+    if __debugLevel>=VERBOSE:
+        print "\033[92m%s%s\033[0m"%("INFO".ljust(10),str(message))
+
+def pwarning(message):
+    if __debugLevel>=WARNING:
+        print "\033[93m%s%s\033[0m"%("WARNING".ljust(10), str(message))
+def perror(message):
+    if __debugLevel>=ERROR:
+        print "\033[91m%s%s\033[0m"%("ERROR".ljust(10),str(message))
